c2218430c689bfce6f466f41df61ea6b66237964
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-11 19:41:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
 # Write your code here.
+
+def badge_maker(name)
+  str = "Hello, my name is #{name}."
+  str.chomp
+  #puts str
+  return str
+end
+
+def batch_badge_creator(array)
+  arr = []
+  #puts "Hello, my name is #{array[0]}"
+  index = 0
+  for i in array
+    str = "Hello, my name is #{array[index]}."
+    arr.push(str)
+    index += 1
+  end
+  puts str
+  return arr
+  #puts str
+end
+
+def assign_rooms(array)
+  arr = []
+  room = 1
+  array.each do |name|
+    str = "Hello, #{name}! You'll be assigned to room #{room}!"
+    arr.push(str)
+    puts str
+    room += 1
+  end
+  return arr
+end
+
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+  #puts assign_rooms(attendees)
+end
